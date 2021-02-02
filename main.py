ee411f859cbbdfbced8c321d48bd344f18071cd3
devkumar24 <dev247kumar@gmail.com>
2021-02-02 23:32:17 +0530

    display suduko board

--- main.py
+++ main.py
@@ -12,25 +12,46 @@ def get_sol(image_path : str = "",model_path : str = "",showGrid = True):
     digit = pre_process_images(image_path)
     grid = extract_no.extract_number(digit,model_path)
     if showGrid:
-        print(grid)
-    grid = extract_no.save_grid(grid,filename="./puzzles.txt")
+        displayBoard(grid)
+#     grid = extract_no.save_grid(grid,filename="./puzzles.txt")
     suduko = suduko_solver.Suduko(filename="./puzzles.txt")
     suduko.solve_suduko()
     solve_grid = suduko.solved_grid
     
-    return solve_grid
+    return grid,solve_grid
+
+def displayBoard(sudoku):
+    for i in range(9):
+        if i % 3 == 0:
+            if i == 0:
+                print(" ┎─────────┰─────────┰─────────┒")
+            else:
+                print(" ┠─────────╂─────────╂─────────┨")
+
+        for j in range(9):
+            if j % 3 == 0:
+                print(" ┃ ", end=" ")
+
+            if j == 8:
+                print(sudoku[i][j] if sudoku[i][j] != 0 else ".", " ┃")
+            else:
+                print(sudoku[i][j] if sudoku[i][j] != 0 else ".", end=" ")
+
+    print(" ┖─────────┸─────────┸─────────┚")
+    
 
 if __name__ == '__main__':
     try:
         start = time.time()
 
-        solved_grid = get_sol(image_path,model_path)
+        grid,solved_grid = get_sol(image_path,model_path)
+        solved_grid = np.array(solved_grid)
         end = time.time()
         
         if -1 in solved_grid:
             print("""Suduko Puzzle has can't be solved due to some unwanted values present in GRID.""")
         else:
-            print(solved_grid)
+            displayBoard(solved_grid)
         print(int(end - start),"Seconds")
         
     except:
